6e23f9ce9a4eb8b3a59526a4bc754ca1aea31ed4
RINKI NAG <rinkinag24@gmail.com>
2018-04-22 23:22:51 +0530

    Update list1.py

--- list1.py
+++ list1.py
@@ -22,10 +22,10 @@
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
  count = 0
-  for word in words:
+ for word in words:
     if len(word) >= 2 and word[0] == word[-1]:
       count+=1
-  return count
+ return count
 
 
 # B. front_x
